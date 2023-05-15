dc7008c0fd346ff5afeeaee111d52a368cfac98b
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-05-15 20:49:29 -0300

    implementando Router

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,18 @@ trazer todos arquivos no src
 configuração de middleware no index
 comando para rodar o servidor;
 a partir disso executar o primeiro teste npm run dev
+Estou fazendo uma configuração de dentro para fora 
+
+
+index >>> banco de dados >> configuração knex >> criação do Router >> 
 */
 
 
 
 import express from 'express'
 import cors from 'cors'
-/* import { productRouter } from './routers/productRouter'
+import { productRouter } from './routers/productsRouter'
+/*
 import { brandRouter } from './routers/brandRouter'
  */
 const app = express()
@@ -21,5 +26,7 @@ app.use(express.json())
 
 app.listen(3003, () => console.log("Server rodando na porta 3003"))
 
-/* app.use("/products", productRouter)
+app.use("/products", productRouter)
+
+/*
 app.use("/brands", brandRouter) */
